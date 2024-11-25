2f2f570a02959c173a01057736cef9c43da9e02f
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-11-25 17:32:18 -0500

    docs: :memo: add Swagger documentation files

---
+++ predictions/static/predictions/docs/swagger-initializer.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  window.ui = SwaggerUIBundle({
+    url: "/static/predictions/docs/swagger.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+};
