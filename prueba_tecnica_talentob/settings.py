f352add6875bd96732a81b07c87b821a328fc75c
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-11-25 21:57:28 -0500

    fix: :bug: remove duplicated labels

--- prueba_tecnica_talentob/settings.py
+++ prueba_tecnica_talentob/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'predictions',
-    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
 ]
 
@@ -41,7 +40,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
